3849e9209b98554a565b869f1057cb4aaec04107
RodionMar <rodionmarmura@gmail.com>
2024-02-14 15:54:39 +0300

    fix: change Footer component

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -8,39 +8,37 @@ import styles from "./index.module.scss";
 const { Footer } = Layout;
 const { Text } = Typography;
 
-const FooterComponent = () => {
-  return (
-    <Footer className={styles.footerStyles}>
-      <Row className={styles.wrapperStyles}>
-        <Col>
-          <Card className={styles.cardStyles} title={<Text className={styles.cardTitleStyles}>Скачать на телефон</Text>}>
-            <Text className={styles.subtitleStyles}>
-              Доступно в PRO-тарифе
-            </Text>
-            <Divider className={styles.divider} />
-            <Row className={styles.buttonContainerStyles}>
-              <Col>
-                <Button type="text" className={styles.cardButtonStyles} icon={<AndroidFilled />}>
-                  Android OS
-                </Button>
-              </Col>
-              <Col>
-                <Button type="text" className={styles.cardButtonStyles} icon={<AppleFilled />}>
-                  Apple iOS
-                </Button>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-        <Col>
-          <Button type="link" className={styles.commentButtonStyles}>
-            Смотреть отзывы
-          </Button>
-        </Col>
-      </Row>
+const FooterComponent = () => (
+  <Footer className={styles.footerStyles}>
+    <Row className={styles.wrapperStyles}>
+      <Col>
+        <Card className={styles.cardStyles} title={<Text className={styles.cardTitleStyles}>Скачать на телефон</Text>}>
+          <Text className={styles.subtitleStyles}>
+            Доступно в PRO-тарифе
+          </Text>
+          <Divider className={styles.divider} />
+          <Row className={styles.buttonContainerStyles}>
+            <Col>
+              <Button type="text" className={styles.cardButtonStyles} icon={<AndroidFilled />}>
+                Android OS
+              </Button>
+            </Col>
+            <Col>
+              <Button type="text" className={styles.cardButtonStyles} icon={<AppleFilled />}>
+                Apple iOS
+              </Button>
+            </Col>
+          </Row>
+        </Card>
+      </Col>
+      <Col>
+        <Button type="link" className={styles.commentButtonStyles}>
+          Смотреть отзывы
+        </Button>
+      </Col>
+    </Row>
+  </Footer >
+)
 
-    </Footer >
-  )
-};
 
 export default FooterComponent;
