605b4afd60917aeef29ba11f3bd6de65fed2bef9
RodionMar <rodionmarmura@gmail.com>
2024-02-21 08:59:55 +0300

    feat: add loader

---
+++ src/components/LottieLoader/LottieLoader.tsx
@@ -0,0 +1,21 @@
+import loaderAnimation from './loader.json';
+import Lottie from 'react-lottie';
+
+import styles from "./index.module.scss";
+
+export default function LottieLoader() {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loaderAnimation,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    };
+
+    return (
+        <div className={styles.wrapper} data-test-id="loader">
+            <Lottie options={defaultOptions} height={150} width={150} />
+        </div>
+    );
+}
