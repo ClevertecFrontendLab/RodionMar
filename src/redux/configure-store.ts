a4d50df7ebe3603d5a4791b77a3e8939710bc4a9
RodionMar <rodionmarmura@gmail.com>
2024-02-21 09:00:49 +0300

    feat: add new configure

--- src/redux/configure-store.ts
+++ src/redux/configure-store.ts
@@ -1,8 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+
+import { createReduxHistoryContext } from "redux-first-history";
+
+import { createBrowserHistory } from "history";
+
+import { useDispatch, TypedUseSelectorHook, useSelector } from "react-redux";
+
+import auth from "@pages/auth/store/auth.slice"
+
+const {
+  createReduxHistory,
+  routerMiddleware,
+  routerReducer
+} = createReduxHistoryContext({ history: createBrowserHistory() });
 
 export const store = configureStore({
-    reducer: {},
+  reducer: {
+    router: routerReducer,
+    auth
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(routerMiddleware),
 });
+
+export const history = createReduxHistory(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
