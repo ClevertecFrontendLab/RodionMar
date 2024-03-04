631de1b8328d29efa82b572cb630c8b3c694bab1
RodionMar <rodionmarmura@gmail.com>
2024-03-04 10:33:24 +0300

    feat: add new functionality for api

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,4 +8,26 @@ const $api = axios.create({
   },
 });
 
+// ===== set token =====
+$api.interceptors.request.use(
+  async (config) => {
+    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// // ===== unathorized interceptor =====
+$api.interceptors.response.use(
+  async (response) => response,
+  (error) => {
+    if (error.response.status === 403) {
+      window.location.replace("/auth");
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default $api;
