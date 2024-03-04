8574aeae254bbf3a3772de1fe9fd74aa8106bd25
RodionMar <rodionmarmura@gmail.com>
2024-03-04 10:44:55 +0300

    feat: add new route

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -3,8 +3,11 @@ import React, { Suspense } from 'react';
 import LottieLoader from '@components/LottieLoader/LottieLoader';
 
 import { Navigate, Routes, Route } from 'react-router-dom';
+
 import AuthPage from '@pages/auth';
 import ResultPage from '@pages/results';
+import FeedbacksPage from '@pages/feedbacks';
+const MainPage = React.lazy(() => import('./pages/main'));
 
 const isAllowed = () => {
     const token = window.localStorage.getItem('token');
@@ -14,11 +17,19 @@ const isAllowed = () => {
     return false;
 };
 
-const PrivateRoute = ({ element: Element }: { element: any }) => {
+const PrivateRouteMainPage = ({ element: Element }: { element: any }) => {
     return isAllowed() ? (
         <Suspense fallback={<LottieLoader />}>
             <Element />
         </Suspense>
+    ) : (
+        <Navigate to={'/'} />
+    );
+};
+
+const PrivateRoute = ({ element: Element }: { element: any }) => {
+    return isAllowed() ? (
+        <Element />
     ) : (
         <Navigate to={'/'} />
     );
@@ -32,17 +43,17 @@ const PublicRoute = ({ element: Element }: { element: any }) => {
     );
 };
 
-const MainPage = React.lazy(() => import('./pages/main'));
-
 const AppRoutes = () => {
     return (
         <Routes>
             {/* PUBLIC */}
             <Route path={'/auth/*'} element={<PublicRoute element={AuthPage} />} />
             <Route path={'/result/*'} element={<PublicRoute element={ResultPage} />} />
+            {/* <Route path={'/feedbacks'} element={<PublicRoute element={FeedbacksPage} />} /> */}
 
             {/* PRIVATE */}
-            <Route path={'/main/*'} element={<PrivateRoute element={MainPage} />} />
+            <Route path={'/main/*'} element={<PrivateRouteMainPage element={MainPage} />} />
+            <Route path={'/feedbacks'} element={<PrivateRoute element={FeedbacksPage} />} />
 
             {/* DEFAULT */}
             <Route path='*' element={<Navigate to='/auth' />} />
