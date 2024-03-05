6d34be5140317083250918090149862f81e5d2a0
RodionMar <rodionmarmura@gmail.com>
2024-03-05 07:57:09 +0300

    fix: change string routes to enum routes

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,13 +1,16 @@
 import React, { Suspense } from 'react';
 
-import LottieLoader from '@components/LottieLoader/LottieLoader';
+import {LottieLoader} from '@components/LottieLoader';
 
 import { Navigate, Routes, Route } from 'react-router-dom';
 
-import AuthPage from '@pages/auth';
-import ResultPage from '@pages/results';
-import FeedbacksPage from '@pages/feedbacks';
+import { AppRouteEnum } from '@constants/app-routes.enum';
+
+import {AuthPage} from '@pages/auth';
+import {ResultPage} from '@pages/results';
+import {FeedbacksPage} from '@pages/feedbacks';
 const MainPage = React.lazy(() => import('./pages/main'));
+
 
 const isAllowed = () => {
     const token = window.localStorage.getItem('token');
@@ -17,46 +20,37 @@ const isAllowed = () => {
     return false;
 };
 
-const PrivateRouteMainPage = ({ element: Element }: { element: any }) => {
+const PrivateRouteMainPage = ({ element: Element }: { element: React.FC }) => {
     return isAllowed() ? (
         <Suspense fallback={<LottieLoader />}>
             <Element />
         </Suspense>
     ) : (
-        <Navigate to={'/'} />
+        <Navigate to={AppRouteEnum.BASIC} />
     );
 };
 
-const PrivateRoute = ({ element: Element }: { element: any }) => {
-    return isAllowed() ? (
-        <Element />
-    ) : (
-        <Navigate to={'/'} />
-    );
+const PrivateRoute = ({ element: Element }: { element: React.FC }) => {
+    return isAllowed() ? <Element /> : <Navigate to={AppRouteEnum.BASIC} />;
 };
 
-const PublicRoute = ({ element: Element }: { element: any }) => {
-    return isAllowed() ? (
-        <Navigate to={'/main'} />
-    ) : (
-        <Element />
-    );
+const PublicRoute = ({ element: Element }: { element: React.FC }) => {
+    return isAllowed() ? <Navigate to={AppRouteEnum.MAIN} /> : <Element />;
 };
 
 const AppRoutes = () => {
     return (
         <Routes>
             {/* PUBLIC */}
-            <Route path={'/auth/*'} element={<PublicRoute element={AuthPage} />} />
-            <Route path={'/result/*'} element={<PublicRoute element={ResultPage} />} />
-            {/* <Route path={'/feedbacks'} element={<PublicRoute element={FeedbacksPage} />} /> */}
+            <Route path={AppRouteEnum.AUTH} element={<PublicRoute element={AuthPage} />} />
+            <Route path={AppRouteEnum.RESULT} element={<PublicRoute element={ResultPage} />} />
 
             {/* PRIVATE */}
-            <Route path={'/main/*'} element={<PrivateRouteMainPage element={MainPage} />} />
-            <Route path={'/feedbacks'} element={<PrivateRoute element={FeedbacksPage} />} />
+            <Route path={AppRouteEnum.MAIN} element={<PrivateRouteMainPage element={MainPage} />} />
+            <Route path={AppRouteEnum.FEEDBACKS} element={<PrivateRoute element={FeedbacksPage} />} />
 
             {/* DEFAULT */}
-            <Route path='*' element={<Navigate to='/auth' />} />
+            <Route path='*' element={<Navigate to={AppRouteEnum.BASIC_AUTH} />} />
         </Routes>
     );
 };
---
+++ src/constants/app-routes.enum.ts
@@ -0,0 +1,8 @@
+export enum AppRouteEnum {
+  BASIC = "/",
+  BASIC_AUTH = "/auth",
+  AUTH = "/auth/*",
+  RESULT = "/result/*",
+  MAIN = "/main/*",
+  FEEDBACKS = "/feedbacks"
+}
