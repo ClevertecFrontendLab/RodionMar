57feabdd89086c67513928c63a0b655b1a6a500d
RodionMar <rodionmarmura@gmail.com>
2024-03-05 08:00:29 +0300

    fix: delete default export from api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,33 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 
 // ===== main instance =====
-const $api = axios.create({
-  baseURL: "https://marathon-api.clevertec.ru",
-  headers: {
-    "Content-Type": "application/json",
-  },
+export const $api = axios.create({
+    baseURL: 'https://marathon-api.clevertec.ru',
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 // ===== set token =====
 $api.interceptors.request.use(
-  async (config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+    async (config) => {
+        config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    },
 );
 
-// // ===== unathorized interceptor =====
+// ===== unathorized interceptor =====
 $api.interceptors.response.use(
-  async (response) => response,
-  (error) => {
-    if (error.response.status === 403) {
-      window.location.replace("/auth");
-    }
-    return Promise.reject(error);
-  }
+    async (response) => response,
+    (error) => {
+        if (error.response.status === 403) {
+            window.location.replace('/auth');
+        }
+        return Promise.reject(error);
+    },
 );
-
-export default $api;
