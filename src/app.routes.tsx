4427d33ad7c8d09ca5c43562ede1b6dc657eaddc
RodionMar <rodionmarmura@gmail.com>
2024-03-18 15:47:53 +0300

    feat: add route to Calendar Page

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -9,6 +9,7 @@ import { AppRouteEnum } from '@constants/app-routes.enum';
 import { AuthPage } from '@pages/auth';
 import { ResultPage } from '@pages/results';
 import { FeedbacksPage } from '@pages/feedbacks';
+import { CalendarPage } from '@pages/calendar';
 const MainPage = React.lazy(() => import('./pages/main'));
 
 const isAllowed = () => {
@@ -50,6 +51,7 @@ const AppRoutes = () => {
                 path={AppRouteEnum.FEEDBACKS}
                 element={<PrivateRoute element={FeedbacksPage} />}
             />
+            <Route path={AppRouteEnum.CALENDAR} element={<PrivateRoute element={CalendarPage} />} />
 
             {/* DEFAULT */}
             <Route path='*' element={<Navigate to={AppRouteEnum.BASIC_AUTH} />} />
