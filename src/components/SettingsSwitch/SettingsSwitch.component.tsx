a9d3f07525b2040069fa0ceef87891f97e45378e
RodionMar <rodionmarmura@gmail.com>
2024-03-27 11:05:01 +0300

    fix: change SettingsSwitchComponent

--- src/components/SettingsSwitch/SettingsSwitch.component.tsx
+++ src/components/SettingsSwitch/SettingsSwitch.component.tsx
@@ -28,40 +28,39 @@ export const SettingsSwitchComponent = ({
     placement,
     switchDataTestId,
     iconDataTestId,
-}: SettingsSwitchProps) => {
-    return (
-        <Row className={styles.switchWrapper} justify='space-between' align='middle' wrap={false}>
-            <Col
-                className={cn(styles.switchTextCol, disabled ? styles.disabledSwitchTextCol : null)}
-            >
-                <Text>{label}</Text>
-                {tooltip && (
-                    <Tooltip
-                        overlayClassName={cn(
-                            styles.settingTooltip,
-                            overlayClassName,
-                            placement !== 'topRight' ? styles.notTopRightTooltip : null,
-                        )}
-                        placement={placement}
-                        title={tooltip}
-                        arrowPointAtCenter
-                    >
-                        <ExclamationCircleOutlined
-                            className={styles.switchTextColIcon}
-                            data-test-id={iconDataTestId}
-                        />
-                    </Tooltip>
-                )}
-            </Col>
-            <Col>
-                <Switch
-                    className={styles.switch}
-                    defaultChecked={defaultChecked}
-                    onChange={onChange}
-                    disabled={disabled}
-                    data-test-id={switchDataTestId}
-                />
-            </Col>
-        </Row>
-    );
-};
+}: SettingsSwitchProps) => (
+    <Row className={styles.switchWrapper} justify='space-between' align='middle' wrap={false}>
+        <Col
+            className={cn(styles.switchTextCol, disabled ? styles.disabledSwitchTextCol : null)}
+        >
+            <Text>{label}</Text>
+            {tooltip && (
+                <Tooltip
+                    overlayClassName={cn(
+                        styles.settingTooltip,
+                        overlayClassName,
+                        placement !== 'topRight' ? styles.notTopRightTooltip : null,
+                    )}
+                    placement={placement}
+                    title={tooltip}
+                    arrowPointAtCenter
+                >
+                    <ExclamationCircleOutlined
+                        className={styles.switchTextColIcon}
+                        data-test-id={iconDataTestId}
+                    />
+                </Tooltip>
+            )}
+        </Col>
+        <Col>
+            <Switch
+                className={styles.switch}
+                defaultChecked={defaultChecked}
+                onChange={onChange}
+                disabled={disabled}
+                data-test-id={switchDataTestId}
+            />
+        </Col>
+    </Row>
+);
+
